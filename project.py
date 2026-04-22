3d69e7b0385592c28fab2dee14fddb2bf0f831fe
Mannat Sadana <mannatsadana@Mannats-MacBook-Pro.local>
2026-04-21 21:20:14 -0400

    Guess number function done

--- project.py
+++ project.py
@@ -14,3 +14,24 @@ def get_difficulty():
             return "Hard",200
         else:
             print("Invalid choice. Please try again.")
+def play_game():
+    level,high=get_difficulty()
+    random_num=random.randint(1,high)
+    attempts=0
+    print(f"\nYou chose {level} mode.")
+    print(f"Guess a number between 1 and {high}.")
+    while True:
+        guess=input(f"Enter your guess between 1 and {high}: ")
+        if guess.isdigit():
+            guess=int(guess)
+            attempts+=1
+            if guess<random_num:
+                print("Too low.")
+            elif guess>random_num:
+                print("Too high.")
+            else:
+                print("Congratulations! You guessed the correct number!")
+                print(f"You guessed it in {attempts} attempts.")
+                break
+        else:
+            print("Invalid input. Please enter a number.")
